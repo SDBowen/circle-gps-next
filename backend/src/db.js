b0daaca1efc6c4cb112c8b62454f8a167eca9ac2
sdbowen <sdbowen.git@gmail.com>
2019-02-03 16:46:46 -0700

    Add graphQL server

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
 // Connection to Prisma DB and schema detail
 
-const { prisma } = require("prisma-binding");
+const { Prisma } = require("prisma-binding");
 
-const db = new prisma({
-  typeDefs: ".src/generated/prisma.graphql",
+const db = new Prisma({
+  typeDefs: "./src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
-  endpoint: process.env.PRISMA_SECRET,
+  secret: process.env.PRISMA_SECRET,
   debug: false
 });
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require("dotenv").config({ path: ".env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO express - handle cookies
+// TODO express - populate user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  connectionEvent => {
+    console.log(`Server is now running on port ${connectionEvent.port}`);
+  }
+);

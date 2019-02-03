bbf292f084c78d64dd84baccfae56a9e6368b5d7
sdbowen <sdbowen.git@gmail.com>
2019-02-02 19:05:14 -0700

    Add prisma

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// Prisma DB connection and schema detail
+
+const { prisma } = require("prisma-binding");
+
+const db = new prisma({
+  typeDefs: ".src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  endpoint: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;

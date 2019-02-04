a20691b2b04abf437be826836b9021dbc638a4ce
sdbowen <sdbowen.git@gmail.com>
2019-02-03 18:06:53 -0700

    Add device model

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+  async createDevice(parent, args, ctx, info) {
+    // TODO check if user logged in
+
+    const device = await ctx.db.mutation.createDevice(
+      {
+        data: { ...args }
+      },
+      info
+    );
+
+    console.log(device);
+
+    return device;
+  }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  devices: forwardTo("db")
+  // async devices(parent, args, ctx, info) {
+  //   const devices = await ctx.db.query.devices();
+
+  //   return devices;
+  // }
+};
 
 module.exports = Query;
